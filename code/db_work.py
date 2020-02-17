a42f946354d9b0f04de6ff305067b1752ba45775
Gleb Nazemnov <nazemnovgleb@gmail.com>
2020-02-17 10:03:35 +0400

    определение курса которого можно перевести

--- code/db_work.py
+++ code/db_work.py
@@ -4,6 +4,8 @@ import json
 
 
 
+can_transition_group = [111,112,113,121,122,131,141,142,151,161,162,211,212,221,231,241,242,251,261,311,312,321,331,341,342,351,361,117,127,147,148,101,102,201,202,203,204,301,302,401,404]
+
 client = pymongo.MongoClient("mongodb://localhost:27017")
 
 print(client)
@@ -24,9 +26,10 @@ def transition_student_to_next_year(id):
         if int(y)>max_y:
             max_y = int(y)
     last_year = years.get(str(max_y))        
-    print(last_year.keys())
-    #создание новго года
-    #алгоритм перевода        
+    group = last_year.get('группа')
+    if int(group) in can_transition_group:
+        print(group)
+          
 
 for student in students.find():
     transition_student_to_next_year(student['_id'])
